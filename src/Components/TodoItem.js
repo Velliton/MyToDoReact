0ed0d2b5c72f40cbc6dc0b5cdc07a50807151554
Velliton <evg92faut@gmail.com>
2022-08-20 15:51:15 +0700

    create component

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todoitem() {
+    return(
+    <div className="todo-item">
+        <input type="checkbox"></input>
+        <p>Убрал кровать?</p>
+    </div>
+    )
+}
+
+export default Todoitem;
+
